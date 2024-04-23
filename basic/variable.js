c659b4916018e27ce9b22fb1996d73ed635b9074
iashutoshyadav <154322101+iashutoshyadav@users.noreply.github.com>
2024-04-23 17:32:10 +0000

    container config

---
+++ basic/variable.js
@@ -0,0 +1,20 @@
+const accountId = 14453
+let accountEmail ="ay0754@gmail.com"
+var accountPassword ="12345"
+accountCity ="Ghaizabad"
+
+// accountId =2
+
+/*
+prefer not to use var
+because of issue in block scope and funtional scope
+
+*/
+
+accountEmail = "chchhvbvhhj@vhbbhj"
+accountPassword = "12345"
+accountCity = "Dehli"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
