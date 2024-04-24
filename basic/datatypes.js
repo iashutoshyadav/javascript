5ba753c50f6370d0d506073defe51c50f8fbdef4
iashutoshyadav <154322101+iashutoshyadav@users.noreply.github.com>
2024-04-24 13:49:34 +0000

    container config

---
+++ basic/datatypes.js
@@ -0,0 +1,24 @@
+"use strict";// treat all JS code as newer version
+
+// alert(3+3)//we are using node js not browser
+
+console.log(3+3)
+//code readability should be high
+console.log("ashutosh")
+
+let name ="vahu"
+let age =20
+let isloggedIn = false
+
+//number => 2 to power 53
+//bight
+//string => ""
+// boolean =>true/false
+//null
+//undefined
+// symbol=>unquie
+
+//object
+
+console.log(typeof age)
+
