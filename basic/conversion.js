6b7b5bd23db0744f544fa592dd698d6c5001e9a4
iashutoshyadav <154322101+iashutoshyadav@users.noreply.github.com>
2024-04-29 15:38:16 +0000

    conversion

---
+++ basic/conversion.js
@@ -0,0 +1,37 @@
+let score = "53"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "54" => 54
+// "33abc"=> NaN
+// true => 1; false=> 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true 0 => false
+// "" => false
+// "vahu" => true
+
+//  operation  //
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+let str1 = "hello"
+let str2 = " vahu"
+let str3 = str1 +str2
+console.log(str3)
+
+console.log(true);
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
