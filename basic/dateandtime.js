ec5c40a8c0f3decb25b0bac1d8cec41721094754
iashutoshyadav <154322101+iashutoshyadav@users.noreply.github.com>
2024-05-13 14:56:07 +0000

    date

---
+++ basic/dateandtime.js
@@ -0,0 +1,16 @@
+  
+  let myDate = new Date()
+  console.log(myDate.toString());
+  console.log(myDate.toDateString);
+  console.log(myDate.toLocaleString);
+  console.log(myDate.toTimeString);
+
+  console.log(typeof myDate);
+
+  let myTimeStamp = Date.now()
+  console.log(myTimeStamp);
+
+  let newDate = new Date ()
+  console.log(newDate);
+  console.log(newDate.getMonth() +1 )
+  console.log(newDate.getDay());
